1be5b83bfed52bde2e3900ae153cdc446020547d
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-12 12:08:18 +0200

    feat(alertService): add more defaults and classNames

--- public/services/AlertService/components/AlertContainer/AlertContainer.tsx
+++ public/services/AlertService/components/AlertContainer/AlertContainer.tsx
@@ -7,16 +7,24 @@ const AlertContainer: React.FC<ToastContainerProps> = ({
 	closeOnClick = false,
 	draggable = false,
 	enableMultiContainer = true,
+	hideProgressBar = true,
+	limit = 1,
 	...rest
 }) => {
 	return (
 		<ToastContainer
 			{...rest}
 			autoClose={autoClose}
+			bodyClassName="o-alert-body"
+			className="o-alert-container"
 			closeButton={closeButton}
 			closeOnClick={closeOnClick}
 			draggable={draggable}
 			enableMultiContainer={enableMultiContainer}
+			hideProgressBar={hideProgressBar}
+			limit={limit}
+			progressClassName="o-alert-progress"
+			toastClassName="o-alert-toast"
 		/>
 	);
 };
