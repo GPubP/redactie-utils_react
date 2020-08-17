cbfff5d9f24475ff32cb2d9ffecefdebfc896d70
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-17 12:18:32 +0200

    fix(alertService): transition flicker by adding custom transition

--- public/services/AlertService/components/AlertContainer/AlertContainer.tsx
+++ public/services/AlertService/components/AlertContainer/AlertContainer.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { ToastContainer, ToastContainerProps } from 'react-toastify';
+import { cssTransition, ToastContainer, ToastContainerProps } from 'react-toastify';
+
+import './AlertContainer.scss';
+
+// Prevent enter/exit flicker because of animation duration
+const NoTransition = cssTransition({
+	enter: 'no-in',
+	exit: 'no-out',
+	duration: 0,
+	collapse: false,
+});
 
 const AlertContainer: React.FC<ToastContainerProps> = ({
 	autoClose = false as false,
@@ -9,6 +19,7 @@ const AlertContainer: React.FC<ToastContainerProps> = ({
 	enableMultiContainer = true,
 	hideProgressBar = true,
 	limit = 1,
+	transition = NoTransition,
 	...rest
 }) => {
 	return (
@@ -25,6 +36,7 @@ const AlertContainer: React.FC<ToastContainerProps> = ({
 			limit={limit}
 			progressClassName="o-alert-progress"
 			toastClassName="o-alert-toast"
+			transition={transition}
 		/>
 	);
 };
