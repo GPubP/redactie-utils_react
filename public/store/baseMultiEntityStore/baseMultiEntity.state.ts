d1f1a6ba2184bf35e17eb5ac1981173502ef5ec5
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-09-09 09:53:45 +0200

    fix: remove commented code

--- public/store/baseMultiEntityStore/baseMultiEntity.state.ts
+++ public/store/baseMultiEntityStore/baseMultiEntity.state.ts
@@ -9,10 +9,6 @@ export interface BaseMultiEntityState<E, IDType>
 	isUpdating: boolean;
 }
 
-// export type getBaseMultiEntityType<S> = S extends BaseMultiEntityState<infer I, infer Y>
-// 	? BaseMultiEntityModel<I, Y>
-// 	: never;
-// export type getBaseMultiIDType<S> = S extends BaseMultiEntityState<any, infer Y> ? Y : never;
 export type GetBaseMultiEntityStateByStore<S> = S extends BaseMultiEntityStore<infer State>
 	? State
 	: never;
