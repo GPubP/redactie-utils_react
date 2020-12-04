3411ac7056f002de6e2d5439e17931661065bc33
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-12-04 10:04:49 +0100

    feat: add some helper functions
    - parseOrderByToString
    - parseStringToOrderBy

--- public/types/index.types.ts
+++ public/types/index.types.ts
@@ -6,6 +6,20 @@ export enum LoadingState {
 	Error = 'error',
 }
 
+export interface SearchParams {
+	page?: number;
+	pagesize?: number;
+	skip?: number;
+	limit?: number;
+	sort?: string;
+	search?: string;
+}
+
+export interface OrderBy {
+	key: string;
+	order: 'asc' | 'desc';
+}
+
 export type HandlerFn = (...args: any[]) => any;
 
 export * from './workers.types';
---
+++ public/utils.helpers.ts
@@ -0,0 +1,24 @@
+import { OrderBy } from './types/index.types';
+
+export const parseOrderByToString = (orderBy: OrderBy): string => {
+	if (!orderBy || typeof orderBy.key !== 'string' || typeof orderBy.order !== 'string') {
+		throw new Error(
+			`orderBy.order and orderBy.key are required fields and must be of type string: ${JSON.stringify(
+				orderBy
+			)}`
+		);
+	}
+	return orderBy.order === 'asc' ? orderBy.key : `-${orderBy.key}`;
+};
+
+export const parseStringToOrderBy = (orderByString = ''): OrderBy => {
+	if (typeof orderByString !== 'string') {
+		throw new Error('orderByString must be of type string');
+	}
+	const keys = orderByString.replace('-', '').split('.');
+	const isDesc = orderByString.includes('-');
+	return {
+		key: keys.length > 1 ? keys[1] : keys[0],
+		order: isDesc ? 'desc' : 'asc',
+	};
+};
