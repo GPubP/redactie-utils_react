9a7ebc6937ef3e480f81fe1e7e9e0449b28e476c
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-02-18 13:24:52 +0100

    test(DeletePrompt): isDeleting prop and add @testing-library/jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	setupFilesAfterEnv: ['<rootDir>/scripts/jest-setup.ts'],
 	// Module file extensions for importing
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	moduleNameMapper: {
