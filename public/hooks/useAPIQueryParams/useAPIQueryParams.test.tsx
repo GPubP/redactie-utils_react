429d95ea682d8f3fac71a5e689109968803f5f05
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-02-25 09:19:46 +0100

    test(useAPIQueryParams): add test for hook

--- public/hooks/useAPIQueryParams/useAPIQueryParams.test.tsx
+++ public/hooks/useAPIQueryParams/useAPIQueryParams.test.tsx
@@ -1,9 +1,18 @@
-import { ArrayParam, DateTimeParam, NumberParam, withDefault } from 'use-query-params';
+import { renderHook } from '@testing-library/react-hooks';
+import { createMemoryHistory } from 'history';
+import React, { ReactElement } from 'react';
+import {
+	ArrayParam,
+	DateTimeParam,
+	NumberParam,
+	QueryParamProvider,
+	withDefault,
+} from 'use-query-params';
 
+import useAPIQueryParams from './useAPIQueryParams';
 import { generateAPIQueryParams } from './useAPIQueryParams.helpers';
-import { APIQueryParamsConfig } from './useAPIQueryParams.types';
 
-const MOCK_CONFIG: APIQueryParamsConfig = {
+const MOCK_CONFIG = {
 	skip: {
 		defaultValue: 10,
 		type: 'number',
@@ -18,7 +27,7 @@ const MOCK_CONFIG: APIQueryParamsConfig = {
 	createdAt: {
 		type: 'dateTime',
 	},
-};
+} as const;
 
 const MOCK_CONFIG_RESULT = {
 	skip: withDefault(NumberParam, 10),
@@ -27,9 +36,33 @@ const MOCK_CONFIG_RESULT = {
 	createdAt: DateTimeParam,
 };
 
+const memoryHistory = createMemoryHistory({ initialEntries: ['/'] });
+
 describe('Hooks: useAPIQueryParams', () => {
 	it('Should generate query params from a given config', () => {
 		const queryParams = generateAPIQueryParams(MOCK_CONFIG);
 		expect(JSON.stringify(queryParams)).toBe(JSON.stringify(MOCK_CONFIG_RESULT));
+	});
+
+	it('Should return a query object and setter', async () => {
+		const wrapper = ({ children }: any): ReactElement => (
+			<QueryParamProvider history={memoryHistory as any}>{children}</QueryParamProvider>
+		);
+		const { result } = renderHook(
+			() =>
+				useAPIQueryParams({
+					search: {
+						type: 'string',
+					},
+				}),
+			{ wrapper }
+		);
+		const [query, setQuery] = result.current;
+
+		expect(query.search).toBeUndefined();
+
+		setQuery({ search: 'query' });
+
+		expect(memoryHistory.location.search).toBe('?search=query');
 	});
 });
