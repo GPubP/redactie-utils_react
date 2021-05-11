9174344eb347d7e28eb88e45ed8f6f6bb9ca3151
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-05-11 15:23:17 +0200

    fix(LeavePrompt): remove modal from index

--- public/components/LeavePrompt/index.ts
+++ public/components/LeavePrompt/index.ts
@@ -1,4 +1,3 @@
 export { default as LeavePrompt } from './LeavePrompt';
-export { default as LeavePromptModal } from './LeavePrompt.modal';
 export * from './LeavePrompt.const';
 export * from './LeavePrompt.types';
