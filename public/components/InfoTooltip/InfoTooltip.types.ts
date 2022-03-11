6ed99b507d8a8a00bc92445111b2eb7653d77c0a
Lotje <lotte-moors@hotmail.com>
2022-03-11 15:35:47 +0100

    chore: remove unnecessary code

--- public/components/InfoTooltip/InfoTooltip.types.ts
+++ public/components/InfoTooltip/InfoTooltip.types.ts
@@ -4,12 +4,3 @@ export interface InfoTooltipProps {
 	type: string;
 }
 
-export enum Status {
-	NEW = 'NEW',
-	DRAFT = 'DRAFT',
-	PENDING_REVIEW = 'PENDING_REVIEW',
-	PENDING_PUBLISH = 'PENDING_PUBLISH',
-	SCHEDULED = 'SCHEDULED',
-	PUBLISHED = 'PUBLISHED',
-	UNPUBLISHED = 'UNPUBLISHED',
-}
