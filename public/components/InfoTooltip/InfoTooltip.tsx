b80df859ed0320593e1fdaf8e556c16fee9ee049
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-04-04 13:32:06 +0200

    feat(info-tooltip): add event

--- public/components/InfoTooltip/InfoTooltip.tsx
+++ public/components/InfoTooltip/InfoTooltip.tsx
@@ -1,12 +1,22 @@
 import { Card, CardBody, Icon } from '@acpaas-ui/react-components';
 import { Tooltip } from '@acpaas-ui/react-editorial-components';
-import React, { FC, useRef, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 
 import { InfoTooltipProps } from './InfoTooltip.types';
 
-const InfoTooltip: FC<InfoTooltipProps> = ({ icon, placement, type, children }): any => {
+const InfoTooltip: FC<InfoTooltipProps> = ({
+	icon,
+	placement,
+	type,
+	children,
+	onVisibilityChange,
+}): any => {
 	const tooltipRef = useRef(null);
 	const [isVisible, setVisibility] = useState(false);
+
+	useEffect(() => {
+		onVisibilityChange && onVisibilityChange(isVisible);
+	}, [isVisible, onVisibilityChange]);
 
 	return (
 		<>
@@ -20,7 +30,7 @@ const InfoTooltip: FC<InfoTooltipProps> = ({ icon, placement, type, children }):
 			</button>
 			<Tooltip placement={placement} isVisible={isVisible} type={type} targetRef={tooltipRef}>
 				<div>
-					<Card style={{ border: 'none', padding:'0.5rem' }}>
+					<Card style={{ border: 'none', padding: '0.5rem' }}>
 						<CardBody style={{ padding: '0.5rem' }}>{children}</CardBody>
 					</Card>
 				</div>
--- public/components/InfoTooltip/InfoTooltip.types.ts
+++ public/components/InfoTooltip/InfoTooltip.types.ts
@@ -2,5 +2,5 @@ export interface InfoTooltipProps {
 	icon: string;
 	placement: string;
 	type: string;
+	onVisibilityChange?: (visible: boolean) => void;
 }
-
