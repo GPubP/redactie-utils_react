820542a7cb6796aa493f821857f296becc6a31dc
Jo Smets <jo.smets@studiohyperdrive.be>
2022-04-05 10:17:58 +0200

    fix(multilang): multilanguage field validation error filter

--- sandbox/src/components/MultilanguageForm/MultilanguageForm.const.ts
+++ sandbox/src/components/MultilanguageForm/MultilanguageForm.const.ts
@@ -9,6 +9,12 @@ export const FORM_VALIDATION_SCHEMA = (languages: any) =>
 				MultilanguageYup.string().required('Beschrijving is een verplicht veld')
 			),
 		}),
+		slug: MultilanguageYup.object().shape({
+			description: MultilanguageYup.object().validateMultiLanguage(
+				languages,
+				MultilanguageYup.string().required('Beschrijving is een verplicht veld')
+			),
+		}),
 		info: MultilanguageYup.object().validateMultiLanguage(
 			languages,
 			MultilanguageYup.string().required('Info is een verplicht veld')
--- sandbox/src/components/MultilanguageForm/MultilanguageForm.mock.ts
+++ sandbox/src/components/MultilanguageForm/MultilanguageForm.mock.ts
@@ -18,5 +18,14 @@ export const LANGUAGE_HEADER_MOCK_LANGUAGES = [
 
 export const LANGUAGE_HEADER_MOCK_TOOLTIP = 'Er zijn opties voor meerdere talen in dit formulier. Selecteer een taal om specifieke instellingen te configureren.';
 
-export const INITIAL_VALUES_MOCK = { title: '', info: { nl: 'nederlands', fr: 'frans', multilanguage: true }, url: {description: { nl: 'nederlands', fr: 'frans', multilanguage: true }}};
+export const INITIAL_VALUES_MOCK = {
+	title: '',
+	info: { nl: 'nederlands', fr: 'frans', multilanguage: true },
+	url: {
+		description: { nl: 'nederlands', fr: 'frans', multilanguage: true }
+	},
+	slug: {
+		description: { nl: 'nederlands', fr: 'frans', multilanguage: true }
+	}
+};
 // export const INITIAL_VALUES_MOCK = { title: '', description: 'test' };
